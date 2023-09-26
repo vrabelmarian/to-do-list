a4f5920c386fa20bd06234472980c6b0e48867fe
marian <mrn.vrabel@gmail.com>
2023-09-26 19:09:24 +0200

    input, add button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form className='new-item-form'>
+      <div className='form-row'>
+        <label htmlFor="item">New item</label>
+        <br/>
+        <input 
+          type="text"
+          id="item"
+          placeholder="learn e.c."></input>
+      </div>
+      <button className="btn" >Add</button>
+    </form>
+  )
 }
-
-export default App;
