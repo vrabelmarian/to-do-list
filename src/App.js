cae81ec0f202543d96ce1d860295e30c5fbf4283
marian <mrn.vrabel@gmail.com>
2023-09-26 19:26:06 +0200

    app components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import './styles.css';
 
 export default function App() {
-  return (
-    <form className='new-item-form'>
-      <div className='form-row'>
-        <label htmlFor="item">New item</label>
-        <br/>
-        <input 
-          type="text"
-          id="item"
-          placeholder="learn e.c."></input>
-      </div>
-      <button className="btn" >Add</button>
-    </form>
-  )
+  return "Hello world"
 }
